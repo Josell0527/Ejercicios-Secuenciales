5f36b39cb71804578253818b8844fd1bf08ae10a
Jose <al_jllaveroi@iessierradelasvillas.org>
2022-09-30 11:15:46 +0200

    Ejercicio 2 es

--- Ejercico 2.py
+++ Ejercico 2.py
@@ -1,10 +1,13 @@
 #Ejercicio 2.Calcular el perímetro y área de un rectángulo dada su base y su altura.
+base=0
+altura= 0 
+
 print ("Cálculo del perimetro de un rectángulo")
 base= (int) (input("Dime la base del rectángulo, \n"))
 altura= (int) (input("Dime la altura del rectángulo, \n"))
 print ("El perímetro del rectángulo es:", ((base*2) + (altura*2)))
 
-print ("Cálculo del area de un rectángulo")
+print ("Cálculo del área de un rectángulo")
 base= (int) (input("Dime la base del rectángulo, \n"))
 altura= (int) (input("Dime la altura del rectángulo, \n"))
 print ("El área del rectángulo es:", (base*altura))
