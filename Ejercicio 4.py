ac1a01a72464c73a37159170da9303f4567670c6
Jose <al_jllaveroi@iessierradelasvillas.org>
2022-10-01 12:26:54 +0200

    Ejercicio 4
    Terminado

---
+++ Ejercicio 4.py
@@ -0,0 +1,10 @@
+#Ejercicio 4. Dados dos números, mostrar la suma, resta, división y multiplicación de ambos
+num1=0
+num2=0
+print("Calculadora")
+num1= (int) (input("Dime un número \n"))
+num2= (int) (input("Dime otro número \n"))
+print("Suma:", (num1+num2))
+print("Resta:", (num1-num2))
+print("Multiplicación:", (num1*num2))
+print("División:", (num1/num2))
