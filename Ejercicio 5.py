1ef091a86cc6ae668284f5b729862573048390e1
Jose <al_jllaveroi@iessierradelasvillas.org>
2022-10-01 12:36:16 +0200

    Ejercicio 5
    Terminado

---
+++ Ejercicio 5.py
@@ -0,0 +1,8 @@
+#Ejercicio5. Escribir un programa que convierta un valor dado en grados Fahrenheit a grados Celsius
+Fahrenheit=0
+Celsius=0
+
+print("Conversor de grados Fahrenheit a Celsius")
+Fahrenheit= (int) (input("Dime los grados Fahrenheit: \n"))
+print("Grados Celsius:", ((Fahrenheit-32)*5/9))
+
