d53f730fa9532080e597e34d7734c5a908a8c1f7
Jose <al_jllaveroi@iessierradelasvillas.org>
2022-10-06 09:35:42 +0200

    Ejercicio 8
    Terminado

--- Ejercicio 8.py
+++ Ejercicio 8.py
@@ -1,14 +1,13 @@
 #Ejercicio 8. Un vendedor recibe un sueldo base mas un 10% extra por comisión de susventas, el vendedor desea saber cuanto dinero obtendrá por concepto de
 #comisiones por las tres ventas que realiza en el mes y el total que recibirá en el mes tomando en cuenta su sueldo base y comisiones.
-base= 0
-comisiones= 0
 venta1= 0
 venta2= 0
 venta3= 0
+salario=0
 
 venta1= (int)(input("Dime los dineros de la venta 1 \n"))
 venta2= (int)(input("Dime los dineros de la venta 2 \n"))
 venta3= (int)(input("Dime los dineros de la venta 3 \n"))
-print("Las comisiones de las ventas han sido de:", ((venta1+venta2+venta3)*1.1), "€")
-print("El sueldo base es de:", (venta1+venta2+venta3))
-p
+salario= (int)(input("Dime el salario: \n"))
+print("Comisiones:", ((venta1+venta2+venta3)*1.1))
+print("El salario total es de:", (((venta1+venta2+venta3)*1.1)+salario), "€")
