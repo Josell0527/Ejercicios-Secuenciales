e2ab16a1aa8706d57a34e8895368326a6dd1cb23
Jose <al_jllaveroi@iessierradelasvillas.org>
2022-10-06 10:05:13 +0200

    Ejercicio 9
    Terminado

---
+++ Ejercicio 9.py
@@ -0,0 +1,4 @@
+#Ejercicio 9. Una tienda ofrece un descuento del 15% sobre el total de la compra y un cliente desea saber cuanto deberá pagar finalmente por su compra.
+compra= 0
+compra= (int)(input("Dime la cuenta de la compra: \n"))
+print("Descuento:", (compra*0.75), "€")
