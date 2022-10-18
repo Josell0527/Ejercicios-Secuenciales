201a89e11eef95e038b627c964a8cd84cce62493
Jose <al_jllaveroi@iessierradelasvillas.org>
2022-10-18 14:41:28 +0200

    Funciones secuenciales

---
+++ EjerciciosFuncionesSecuenciales.py
@@ -0,0 +1,5 @@
+#Ejercicio1. Escribir un programa que pregunte al usuario su nombre, y luego lo salude
+def ej1():
+    nombre= ""
+    nombre= input("Dime tu nombre, \n")
+    print("Hola", nombre)
