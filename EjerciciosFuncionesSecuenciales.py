893d475159e05307a3f92e4f144940ed08e464e0
Jose <al_jllaveroi@iessierradelasvillas.org>
2022-10-20 10:14:41 +0200

    nuevo ejercicios secuenciales

--- EjerciciosFuncionesSecuenciales.py
+++ EjerciciosFuncionesSecuenciales.py
@@ -3,3 +3,16 @@ def ej1():
     nombre= ""
     nombre= input("Dime tu nombre, \n")
     print("Hola", nombre)
+def ej2():
+    base=0
+    altura= 0 
+
+    print ("Cálculo del perimetro de un rectángulo")
+    base= (int) (input("Dime la base del rectángulo, \n"))
+    altura= (int) (input("Dime la altura del rectángulo, \n"))
+    print ("El perímetro del rectángulo es:", ((base*2) + (altura*2)))
+
+    print ("Cálculo del área de un rectángulo")
+    base= (int) (input("Dime la base del rectángulo, \n"))
+    altura= (int) (input("Dime la altura del rectángulo, \n"))
+    print ("El área del rectángulo es:", (base*altura))
